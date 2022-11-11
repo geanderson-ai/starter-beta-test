52942a8bf1836ccb29f01a460c28451be6e3f514
Scott Motte <mot@mot.la>
2022-11-11 08:35:52 -0800

    Remove from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const result = require('dotenv-vault-core').config()
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: result.parsed
 }
 
 module.exports = nextConfig
